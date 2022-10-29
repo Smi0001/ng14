9d0f049e07a2b84ec8c29a9cd727167a542a7547
Smi0001 <shammi.hans@coverfoxmail.com>
2022-10-29 13:43:30 +0530

    begin, added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
+import { PopupComponent } from './popup/popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    ProfileCardComponent,
+    CardComponent,
+    ButtonComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
